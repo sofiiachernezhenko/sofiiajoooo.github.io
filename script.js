98ae037a29f432c5d5587c5037602a67aeb56245
sofiiachernezhenko <sofiiachernezhenko@gmail.com>
2026-02-13 14:34:43 +0100

    Add more photo paths to the photos array

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
     'images/photo1.jpg',
     'images/photo2.jpg',
     'images/photo3.jpg',
+    'images/photo4.jpg',
+    'images/photo5.jpg',
+    'images/photo6.jpg',
+    'images/photo7.jpg',
+    'images/photo8.jpg',
   ];
 
     const slidesContainer = document.getElementById('slides');
